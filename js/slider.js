b3c54165dc743940201ad1f279fb0f7e4722d04b
josephmalete1997 <josephmalete75@gmail.com>
2023-09-16 00:38:37 +0000

    updates

---
+++ js/slider.js
@@ -0,0 +1,5 @@
+let sliderImage = document.querySelector(".slider");
+
+sliderImage.style.backgroundImage = "url(images/room.jpeg)";
+sliderImage.style.backgroundPositionY = "-400px";
+sliderImage.style.backgroundRepeat = "no-repeat";
